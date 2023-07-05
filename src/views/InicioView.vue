08c145604d4173327f88168c633011a2dcb20682
Kevin Muñoz <harquin09@gmail.com>
2023-07-05 16:57:50 -0500

    mejorando

--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -1,7 +1,26 @@
 <script setup>
 import Receta from "../components/Receta.vue";
+import { ref, watch, computed } from "vue";
 import { useBebidasStore } from "../stores/bebidas";
 const bebidasStore = useBebidasStore();
+const filtroLocal = ref("");
+const dataLocal = ref([]);
+
+watch(
+  () => bebidasStore.cocktails,
+  (cocktails) => {
+    dataLocal.value = cocktails;
+  }
+);
+const bebidasFiltradas = computed(() => {
+  if (!filtroLocal.value) {
+    return dataLocal.value;
+  }
+  const filtro = filtroLocal.value.toLowerCase();
+  return dataLocal.value.filter((bebida) =>
+    bebida.strDrink.toLowerCase().includes(filtro)
+  );
+});
 </script>
 <template>
   <h1 class="text-6xl font-extrabold text-center">Recetas</h1>
@@ -20,6 +39,8 @@ const bebidasStore = useBebidasStore();
         class="border border-gray-300 rounded-lg px-5 py-2 w-1/2"
         type="text"
         placeholder="Buscar bebida"
+        v-model="filtroLocal"
+        @input="filtrarDataLocal"
       />
     </div>
   </div>
@@ -27,7 +48,7 @@ const bebidasStore = useBebidasStore();
     class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 mt-10 my-10 gap-10"
   >
     <Receta
-      v-for="cocktail in bebidasStore.cocktails"
+      v-for="cocktail in bebidasFiltradas"
       :cocktail="cocktail"
       :key="cocktail.idDrink"
     />
